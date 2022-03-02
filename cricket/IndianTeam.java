733b2dfc614dcbb5e21e43bebb3b91a71dde6aec
ManoharD25 <manohar.xworkz@gmail.com>
2022-03-02 16:04:34 +0530

    just for fractice

---
+++ cricket/IndianTeam.java
@@ -0,0 +1,15 @@
+package cricket;
+
+public class IndianTeam {
+	
+	
+	static String player1 = "VK";
+	static int players = 11;
+	
+	
+	static void Play(){
+		
+		System.out.println("He is playing ");
+	}
+
+}
---
+++ cricket/Test.java
@@ -0,0 +1,17 @@
+package cricket;
+
+public class Test {
+
+	
+	public static void main(String []args) {
+		
+		IndianTeam.Play();
+		
+		String name1 = IndianTeam.player1;
+		
+		System.out.println(name1="KL");
+		
+		int  player2 = IndianTeam.players;
+		System.out.println(player2);
+	}
+}
